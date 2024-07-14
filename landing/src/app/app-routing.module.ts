8cc55a82f86ea8044657b8529c6007c341ad6cd4
JoaoVitorJorgeMilani <joaovjmilani@gmail.com>
2024-07-14 14:21:58 -0300

    testes primeira versao

--- landing/src/app/app-routing.module.ts
+++ landing/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: HomeComponent,
+    // children: [
+    //   {
+    //     path: '',
+    //     component: HomeMenuComponent
+    //   },
+    //   {
+    //     path: 'about',
+    //     component: HomeAboutComponent
+    //   }
+    // ]  
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- landing/src/app/app.component.ts
+++ landing/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'landing';
+
+  constructor(private router: Router) { }
+  
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const body = document.getElementsByTagName('body')[0];
+        const scrollTo = document.querySelector('#' + this.router.url.substring(2));
+        if (scrollTo) {
+          body.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+          scrollTo.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+        }
+      });
+  }
 }
